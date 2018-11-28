75f51b366bbbc337dec4104506277372d60fafdc
Troy Larson <troylar@gmail.com>
2018-11-28 11:10:08 -0500

    Preparing release 0.1.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kitchen-sink-logger",
-    version='0.0.10',
+    version='0.1.0',
     author="Troy Larson",
     author_email="troylar@gmail.com",
     description=(
